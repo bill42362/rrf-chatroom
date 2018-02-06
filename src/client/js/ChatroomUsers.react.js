1086a060c2ccae7168c548e5d6ea7c69fde565f6
Bill42362 <bill42362@gmail.com>
2018-02-07 00:12:47 +0800

    Display formated date and fix overflow issue in ChatroomUsers.

--- src/client/js/ChatroomUsers.react.js
+++ src/client/js/ChatroomUsers.react.js
@@ -4,13 +4,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase';
+import { getDateStringWithFormat } from './Utils.js';
 import '../css/chatroom-users.less';
 
+const MSEC_IN_ONE_DAY = 3600*24*1000;
+
+const onlineIndicator = <div className='chatroom-user-online'>・</div>;
 class ChatroomUsers extends React.Component {
     constructor(props) { super(props); }
     render() {
         const { users, userName } = this.props;
-        const onlineIndicator = <div className='chatroom-user-online'>・</div>;
+        const now = Date.now();
         const userList = !isLoaded(users)
             ? <div className='mock-user'>Loading ...</div>
             : isEmpty(users)
@@ -18,7 +22,11 @@ class ChatroomUsers extends React.Component {
                 : Object.keys(users).filter(user => userName !== user).map((key, index) => {
                     const user = users[key];
                     const oddClassName = 0 === index%2 ? ' chatroom-user-odd' : '';
-                    const lastOnline = <div className='chatroom-user-lastOnline'>{user.lastOnline}</div>;
+                    const shouldDisplayDate = MSEC_IN_ONE_DAY < now - user.lastOnline;
+                    const lastOnline = <div className='chatroom-user-lastOnline'>
+                        {shouldDisplayDate && `${getDateStringWithFormat({timestamp: user.lastOnline, format: 'MM/DD'})} `}
+                        {getDateStringWithFormat({timestamp: user.lastOnline, format: 'hh:mm'})}
+                    </div>;
                     return <div className={`chatroom-user${oddClassName}`} key={index}>
                         <div className='chatroom-user-name'>{key}</div>
                         {user.connections ? onlineIndicator : lastOnline}
