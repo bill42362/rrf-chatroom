f03c6aab543827ec0748fd553c52353ebbecc404
Bill42362 <bill42362@gmail.com>
2018-02-06 19:33:01 +0800

    Isolate UserNameInput from App.

--- src/client/js/App.react.js
+++ src/client/js/App.react.js
@@ -2,42 +2,20 @@
 'use strict';
 import React from 'react';
 import { connect } from 'react-redux';
-import Debug from 'debug';
+import UserNameInput from './UserNameInput.react.js';
 import User from './User.react.js';
-import { Actions as UserActions } from './User.js';
 import ChatroomUsers from './ChatroomUsers.react.js';
 import Messages from './Messages.react.js';
 import MessageInput from './MessageInput.react.js';
 import '../css/app.less';
 
-Debug.disable();
-if('production' != process.env.NODE_ENV) { Debug.enable('rrf-chatroom:*'); }
-
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onKeyDown = this.onKeyDown.bind(this);
-    }
-    onKeyDown(e) {
-        if(13 === e.keyCode && !e.shiftKey) {
-            e.preventDefault();
-            const { updateName, editingName } = this.props;
-            updateName({name: editingName.trim()})
-            return false;
-        }
-    }
+    constructor(props) { super(props); }
     render() {
-        const { name, editingName, updateName, updateEditingName } = this.props;
+        const { name } = this.props;
         return <div className='app'>
             {!name && <div className='app-user-name-input-wrapper'>
-                <div className='app-user-name-input'>
-                    <div className='app-user-name-input-description'>Please enter your nickname.</div>
-                    <input
-                        className='app-user-name-input' autoFocus
-                        value={editingName} onChange={e => { updateEditingName({editingName: e.target.value}); }}
-                        onKeyDown={this.onKeyDown}
-                    />
-                </div>
+                <UserNameInput />
             </div>}
             {name && <div className='info'>
                 <div className='user-wrapper'>
@@ -63,15 +41,6 @@ export default connect(
     (state, ownProps) => {
         return {
             name: state.user.name,
-            editingName: state.user.editingName,
         };
-    },
-    (dispatch, ownProps) => ({
-        updateName: ({ name }) => {
-            return dispatch(UserActions.updateName({ name }));
-        },
-        updateEditingName: ({ editingName }) => {
-            return dispatch(UserActions.updateEditingName({ editingName }));
-        },
-    })
+    }
 )(App);
