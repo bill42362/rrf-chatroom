3bb41fcf0e3c3a7a0e4c918bdb8e8a5159852b19
Bill42362 <bill42362@gmail.com>
2018-02-07 12:27:58 +0800

    Add LayoutVars to collapse ChatroomUsers on mobile.

--- src/client/js/App.react.js
+++ src/client/js/App.react.js
@@ -2,6 +2,7 @@
 'use strict';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Actions as LayoutVarsActions } from './LayoutVars.js';
 import UserNameInput from './UserNameInput.react.js';
 import User from './User.react.js';
 import ChatroomUsers from './ChatroomUsers.react.js';
@@ -12,16 +13,28 @@ import '../css/app.less';
 class App extends React.Component {
     constructor(props) { super(props); }
     render() {
-        const { name } = this.props;
+        const {
+            name, chatroomUsersHeight,
+            shouldCollapseChatroomUsers, updateShouldCollapseChatroomUsers
+        } = this.props;
+        const chatroomUsersWrapperHeight = shouldCollapseChatroomUsers
+            ? '0px'
+            : `${chatroomUsersHeight + 5}px`;
         return <div className='app'>
             {!name && <div className='app-user-name-input-wrapper'>
                 <UserNameInput />
             </div>}
             {name && <div className='info'>
                 <div className='user-wrapper'>
+                    <div
+                        className='shoule-collapse-chatroom-users-toggler' role='button'
+                        onClick={() => updateShouldCollapseChatroomUsers({
+                            shouldCollapseChatroomUsers: !shouldCollapseChatroomUsers
+                        })}
+                    >O</div>
                     <User />
                 </div>
-                <div className='chatroom-users-wrapper'>
+                <div className='chatroom-users-wrapper' style={{height: chatroomUsersWrapperHeight}}>
                     <ChatroomUsers />
                 </div>
             </div>}
@@ -41,6 +54,13 @@ export default connect(
     (state, ownProps) => {
         return {
             name: state.user.name,
+            chatroomUsersHeight: state.layoutVars.chatroomUsersHeight,
+            shouldCollapseChatroomUsers: state.layoutVars.shouldCollapseChatroomUsers,
         };
-    }
+    },
+    (dispatch, ownProps) => ({
+        updateShouldCollapseChatroomUsers: ({ shouldCollapseChatroomUsers }) => {
+            return dispatch(LayoutVarsActions.updateLayoutVars({layoutVars: { shouldCollapseChatroomUsers }}));
+        },
+    })
 )(App);
