c4828c20df6532fd3ac591d3ff78e1d607fb3825
Bill42362 <bill42362@gmail.com>
2018-02-28 20:46:19 +0800

    Done sync self/remote firebase peer packs.

--- src/client/js/Rtc.js
+++ src/client/js/Rtc.js
@@ -8,69 +8,120 @@ const localPeerPacks = {};
 let selfRtcClientRef = undefined;
 let recevingClientIdsRef = undefined;
 let rtcClientsRef = undefined;
+let peerPacksRef = undefined;
 
-const rtcDataTemplate = {
-    userName: '',
-    clientId: '',
-    offeringUsers: [],
-    recevingUsers: [],
-
-    offeredClientIds: [],
-    recevingClientIds: [],
-
-    offeredUsers: [],
+const defaultState = {
+    peerPacks: [],
+    clientId,
 };
 
-const peerPackTemplate = {
+const rtcDataTemplate = {
+    userName: '', clientId: '',
+    offeringUsers: [], recevingUsers: [],
+    offeredClientIds: [], recevingClientIds: [],
+    offeredUsers: [],
+    peerPacks: [],
+};
+
+const localPeerPackTemplate = {
     remoteClientId: '',
     peerConnection: undefined,
+    isAnswerPeer: false,
+};
+
+const storePeerPackTemplate = {
+    remoteClientId: '',
+    remoteUserName: '',
+    stream: undefined,
+    isSendingVideo: true, isSendingAudio: true,
+    isRecevingVideo: true, isRecevingAudio: true,
+};
+
+const firebasePeerPackTemplate = {
+    remoteClientId: '',
     isAnswerPeer: false,
     icecandidate: undefined,
     peerDescription: undefined,
 };
+
+export const Reducer = (state = defaultState, action) => {
+    switch(action.type) {
+        case 'UPDATE_USER_EDITING_NAME':
+            return state;
+        default:
+            return state;
+    }
+}
 
 class LocalPeerPack {
     constructor({ remoteClientId, isAnswerPeer, peerConnectionConfig }) {
         this.remoteClientId = remoteClientId;
         this.isAnswerPeer = isAnswerPeer;
         this.peerConnection = createPeerConnection(peerConnectionConfig);
+    }
+    setSendingMedia({ audio, video }) {
         if(this.peerConnection) {
-            navigator.mediaDevices.getUserMedia({audio: true, video: true})
+            return navigator.mediaDevices.getUserMedia({ audio, video })
                 .then(stream => {
                     this.peerConnection.addStream(stream);
-                    if(isAnswerPeer) {
+                    if(this.isAnswerPeer) {
                         return this.peerConnection.createAnswer();
                     } else {
                         return this.peerConnection.createOffer();
                     }
                 })
                 .then(createdDescription => {
-                    this.description = createdDescription;
-                    return this.peerConnection.setLocalDescription(createdDescription);
+                    return this.peerConnection.setLocalDescription(createdDescription)
+                        .then(() => createdDescription);
                 })
-                .then(() => {
-                    if(this.ondescription) { this.ondescription(this.description); };
+                .then(createdDescription => {
+                    if(this.ondescription) { this.ondescription(createdDescription); };
                 })
                 .catch(error => console.log);
         } else {
-            throw new Error('createPeerConnection() fail.');
+            throw new Error('LocalPeerPack.setSendingMedia() peerConnection not existed.');
         }
     }
 };
 
-const getRtcData = ({ firebase, roomName, userName, clientId }) => {
+class StorePeerPack {
+    constructor({ firebase, remoteClientId }) {
+        const database = firebase.database();
+        this.remoteClientId = remoteClientId;
+        this.remoteClientRef = database.ref(`${roomName}/rtcClients/${clientId}`);
+        this.remoteClientRef.once('value')
+            .then(snapshot => {
+                const remoteRtcData = snapshot.val();
+                this.remoteUserName = removedRtcData.userName;
+            })
+            .catch(error => console.log);
+    }
+};
+
+const getRtcData = ({ userName, clientId }) => {
     return Object.assign({}, rtcDataTemplate, {
         userName, clientId
     });
+};
+
+const addPeerPacksToFirebase = ({ firebase, roomName, remoteClientId }) => {
+    const database = firebase.database();
+    const selfPeerPackRef = database.ref(`${roomName}/rtcClients/${clientId}/peerPacks/${remoteClientId}`);
+    const remotePeerPackRef = database.ref(`${roomName}/rtcClients/${remoteClientId}/peerPacks/${clientId}`);
+    selfPeerPackRef.set({isAnswerPeer: true, remoteClientId });
+    remotePeerPackRef.set({remoteClientId: clientId, isAnswerPeer: false});
+    selfPeerPackRef.onDisconnect().remove();
+    remotePeerPackRef.onDisconnect().remove();
 };
 
 export const createRtcData = ({ firebase, roomName, userName }) => new Promise((resolve, reject) => {
     const database = firebase.database();
     rtcClientsRef = database.ref(`${roomName}/rtcClients`);
     selfRtcClientRef = database.ref(`${roomName}/rtcClients/${clientId}`);
-    selfRtcClientRef.set(getRtcData({ firebase, roomName, userName, clientId }));
+    selfRtcClientRef.set(getRtcData({ userName, clientId }));
     selfRtcClientRef.onDisconnect().remove();
     recevingClientIdsRef = database.ref(`${roomName}/rtcClients/${clientId}/recevingClientIds`);
+    peerPacksRef = database.ref(`${roomName}/rtcClients/${clientId}/peerPacks`);
 
     rtcClientsRef.on('child_removed', snapshot => {
         const removedRtcData = snapshot.val();
@@ -94,6 +145,16 @@ export const createRtcData = ({ firebase, roomName, userName }) => new Promise((
 
     recevingClientIdsRef.on('child_added', snapshot => {
         const addedRecevingClientId = snapshot.val();
+        const isAnswerPeer = clientId < addedRecevingClientId;
+        if(isAnswerPeer) {
+            addPeerPacksToFirebase({remoteClientId: addedRecevingClientId, roomName, firebase });
+        }
+    });
+
+    peerPacksRef.on('child_added', snapshot => {
+        const addedPeerPack = snapshot.val();
+        console.log('peerPacksRef.child_added() addedPeerPack:', addedPeerPack);
+        /*
         const localPeerPack = new LocalPeerPack({
             remoteClientId: addedRecevingClientId,
             isAnswerPeer: false,
@@ -104,10 +165,14 @@ export const createRtcData = ({ firebase, roomName, userName }) => new Promise((
         localPeerPack.peerConnection.onicecandidate = event => {
             console.log('onicecandidate() candidate:', event.candidate);
         };
+        localPeerPack.setSendingMedia({audio: true, video: true});
         localPeerPacks[addedRecevingClientId] = localPeerPack;
+         */
     });
 
     resolve({ firebase, roomName, userName });
 });
 
-export default { createRtcData };
+export const Actions = { };
+
+export default { Reducer, Actions, createRtcData };
